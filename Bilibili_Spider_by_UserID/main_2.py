38f93c853d8032d91abe9aa3327d36bb4c2ff385
Alexis <Alexis_Yang_@outlook.com>
2024-10-17 15:32:01 +0800

    commit

---
+++ Bilibili_Spider_by_UserID/main_2.py
@@ -0,0 +1,27 @@
+import os
+import os.path as osp
+import argparse
+
+from utils.bilibili_spider import Bilibili_Spider
+
+
+def sub_main(args):
+    bilibili_spider = Bilibili_Spider(args.uid, args.save_dir, args.save_by_page, args.time)
+    bilibili_spider.get()
+    if args.detailed:
+        bilibili_spider.get_detail()
+
+
+def bilibili(uid='362548791'):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--uid', type=str, default=uid)  #'362548791'
+    parser.add_argument('--save_dir', type=str, default='json')
+    parser.add_argument('--save_by_page', action='store_true', default=False)
+    parser.add_argument('--time', type=int, default=2, help='waiting time for browser.get(url) by seconds')
+    parser.add_argument('--detailed', action='store_true', default=False)
+    args = parser.parse_args()
+    # print(args)
+    sub_main(args)
+
+def main():
+    bilibili()
